46a930553ff7120fba44cd0100f4f1f897c68d69
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-07-31 18:39:59 +0400

    worked on adding Leaflet

--- src/components/MainMap/MainMap.js
+++ src/components/MainMap/MainMap.js
@@ -1,15 +1,32 @@
-import React, { Component } from './node_modules/react'
-import { Map } from './node_modules/react-leaflet'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import styles from "./mainmap.module.css"
 
-export default class MyMap extends Component {
+export default class MainMap extends Component {
+    static propTypes = {
+        position: PropTypes.array,
+        zoom: PropTypes.number,
+        markerText: PropTypes.string
+    }
+    static defaultProps = {
+        position: [51, -1],
+        zoom: 13,
+        markerText: ""
+    }
     render() {
         const { options } = this.props
 
         if (typeof window !== 'undefined') {
             return (
-                <Map {...options}>
-
-                </Map>
+                <div id={styles.mainMapId}>
+                    <Map center={this.props.position} zoom={this.props.zoom}>
+                        <TileLayer
+                            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                        />
+                    </Map>
+                </div>
             )
         }
         return null
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from 'gatsby'
 import Layout from "../components/Layout.js"
+import MainMap from "../components/MainMap/MainMap"
 import "./styles/index.css"
 
 export default () => {
@@ -12,6 +13,7 @@ export default () => {
         </section>
         <section className="map-section">
           <p>Map</p>
+          <MainMap />
         </section>
       </div>
     </Layout>
