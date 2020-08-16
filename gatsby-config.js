7f0bb456886268652fb03735c6eefde848c055e5
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-08-16 16:05:10 +0400

    fixed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
         linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
       }
     },
+    `gatsby-transformer-remark`,
   ],
   siteMetadata: {
     title: `GeoBeerMap`,
