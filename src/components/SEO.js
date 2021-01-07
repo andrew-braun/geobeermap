96dcb5a7a5d6aa08d988df0c35f8f8fdeedb8a1a
Andrew Braun <andrew@andrewbraun.dev>
2021-01-07 22:14:45 +0400

    seo: Update description tag to be capitalized (step 3)

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -29,7 +29,7 @@ function SEO({ description, lang, meta, title, image }) {
 			titleTemplate={`%s | ${site.siteMetadata.title}`}
 			meta={[
 				{
-					name: `Descriptionzz`,
+					name: `Description`,
 					content: metaDescription,
 				},
 				{
