9e1f814d57289350afab0412d7dbc84583964ab1
Andrew Braun <andrew@andrewbraun.dev>
2021-01-28 22:39:15 +0400

    refactor: Resolve ESLint warnings in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import { AiOutlineGithub } from "react-icons/ai"
 import "../../styles/global.css"
 import styles from "./footer.module.css"
@@ -18,23 +18,33 @@ const Footer = () => {
 		`
 	)
 
-	const { title, subtitle } = data.site.siteMetadata
+	const { title } = data.site.siteMetadata
 
 	return (
 		<footer className={styles.mainFooter}>
 			<div className={styles.footerInfo}>
 				<span className={styles.siteTitle}>{title}</span> - made with
-				<span className="emoji">
-					&#128150; and &#127866; by&nbsp;
-					<a href="https://andrewbraun.dev" target="_blank">
-						Andrew Braun
-					</a>
+				<span className="emoji" role="img" aria-label="emoji">
+					&#128150;
 				</span>
+				and
+				<span className="emoji" role="img" aria-label="emoji">
+					&#127866;
+				</span>
+				by&nbsp;
+				<a
+					href="https://andrewbraun.dev"
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					Andrew Braun
+				</a>
 			</div>
 			<div className={styles.footerLinks}>
 				<a
 					href="https://github.com/andrew-braun/geobeermap"
 					target="_blank"
+					rel="noreferrer noopener"
 					className={styles.footerLink}
 				>
 					<AiOutlineGithub />
