d49aeb8af4c44f37dfdb28ea1105f7f4d1d21f99
Andrew Braun <andrew@andrewbraun.dev>
2021-06-23 10:40:38 +0400

    feat: Set up Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,6 +103,17 @@ module.exports = {
 		`gatsby-plugin-sitemap`,
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				// You can add multiple tracking ids and a pageview event will be fired for all of them.
+				trackingIds: [
+					"G-GXZ1DG394M", // Google Analytics / GA
+					// "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+					// "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+				],
+			},
+		},
 	],
 	siteMetadata: {
 		title: `GeoBeerMap`,
