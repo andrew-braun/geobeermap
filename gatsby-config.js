c6523710c11632e249ec8ec7068224210bfa5e92
Andrew Braun <andrew@andrewbraun.dev>
2021-06-23 10:42:30 +0400

    feat: Add anonymization/DNT to GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,9 +109,23 @@ module.exports = {
 				// You can add multiple tracking ids and a pageview event will be fired for all of them.
 				trackingIds: [
 					"G-GXZ1DG394M", // Google Analytics / GA
-					// "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-					// "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+					"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+					"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
 				],
+			},
+			gtagConfig: {
+				optimize_id: "OPT_CONTAINER_ID",
+				anonymize_ip: true,
+				cookie_expires: 0,
+			},
+			// This object is used for configuration specific to this plugin
+			pluginConfig: {
+				// Puts tracking script in the head instead of the body
+				head: false,
+				// Setting this parameter is also optional
+				respectDNT: true,
+				// Avoids sending pageview hits from custom paths
+				exclude: ["/preview/**", "/do-not-track/me/too/"],
 			},
 		},
 	],
