b8e7bc8af16b2f212ba1082bab67ef1670232a5c
Andrew Braun <andrew@andrewbraun.dev>
2022-08-07 12:37:02 +0400

    style: Set map to fullscreen

--- components/maps/MainMap/MainMap.js
+++ components/maps/MainMap/MainMap.js
@@ -10,10 +10,11 @@ export default function MainMap({}) {
 	const [viewport, setViewport] = useState({
 		latitude: 41.704202197992004,
 		longitude: 44.77889107691885,
-		zoom: 9,
+		zoom: 11,
 		initialViewport: true,
 	})
 
+	// Run only on first page load when location request is made; when permission is granted, move and zoom
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition((pos) => {
 			setViewport({
@@ -21,7 +22,7 @@ export default function MainMap({}) {
 				...viewport,
 				latitude: pos.coords.latitude,
 				longitude: pos.coords.longitude,
-				zoom: 3.5,
+				zoom: 13,
 				initialViewport: false,
 			})
 		})
@@ -30,20 +31,26 @@ export default function MainMap({}) {
 
 	return (
 		<div className={`${styles.mainMap}`}>
-			<Map
-				initialViewState={{
-					latitude: viewport.latitude,
-					longitude: viewport.longitude,
-					zoom: 11,
-				}}
-				style={{ width: 800, height: 400 }}
-				mapStyle="mapbox://styles/andrew-braun/cl6hupilg000515jyytsivq9d"
-				mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-			>
-				{!viewport.initialViewport && (
-					<Marker longitude={viewport.longitude} latitude={viewport.latitude} />
-				)}
-			</Map>
+			<div className={`${styles.mapWrapper}`}>
+				<Map
+					initialViewState={{
+						latitude: viewport.latitude,
+						longitude: viewport.longitude,
+						zoom: viewport.zoom,
+					}}
+					style={{ minHeight: "80vh" }}
+					onViewportChange={(viewport) => setViewport(viewport)}
+					mapStyle="mapbox://styles/andrew-braun/cl6hv3y3z003d15o9ktycfewt"
+					mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+				>
+					{!viewport.initialViewport && (
+						<Marker
+							longitude={viewport.longitude}
+							latitude={viewport.latitude}
+						/>
+					)}
+				</Map>
+			</div>
 		</div>
 	)
 }
