adfd049d574291be218ab31caf882150a01a4d19
andrew-braun <andrew@andrewbraun.dev>
2023-07-24 21:20:53 +0400

    fix: Fix social icon sizing issue

--- components/SocialLinks/SocialLink.js
+++ components/SocialLinks/SocialLink.js
@@ -4,10 +4,13 @@ import styles from "./SocialLink.module.scss"
 
 export default function SocialLink({ link, icon, size }) {
 	return (
-        (<Link href={link} target="_blank" className={`${styles.link}`}>
-
-            {icon}
-
-        </Link>)
-    );
+		<Link
+			href={link}
+			target="_blank"
+			className={`${styles.link}`}
+			style={{ width: parseInt(size) * 2, height: parseInt(size) * 2 }}
+		>
+			{icon}
+		</Link>
+	)
 }
