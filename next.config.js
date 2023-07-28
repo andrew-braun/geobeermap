8284535673f27d2477f11c2dc0953f4c46462dfa
andrew-braun <andrew@andrewbraun.dev>
2023-07-28 13:05:54 +0400

    seo: Add redirects for all venues

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const { generateNewSlugRedirects } = require("./lib/data/site/redirects")
+
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
 		domains: ["res.cloudinary.com"],
 		// loader: "cloudinary",
 		// path: "res.cloudinary.com/geobeermap",
+	},
+	redirects: async () => {
+		const newSlugRedirects = await generateNewSlugRedirects()
+
+		return [...newSlugRedirects]
 	},
 }
 
