a4ff25e08f2141a96288516f9c3f73ad94b8b45f
andrew-braun <andrew@andrewbraun.dev>
2023-07-28 14:39:23 +0400

    chore: Remove unnecessary console.logs

--- components/forms/ContactForm.js
+++ components/forms/ContactForm.js
@@ -50,7 +50,6 @@ export default function ContactForm() {
 	}
 
 	useEffect(() => {
-		console.log(isSubmitSuccessful)
 		if (isSubmitSuccessful) {
 			toggleModal()
 		}
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ venues }) {
 		url: siteUrl,
 		description: siteDescription,
 	} = siteData
-	console.log(venues)
+
 	return (
 		<>
 			<NextSeo
