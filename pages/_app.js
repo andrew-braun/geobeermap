32c4e5fbc62ada795002c861937efc2c22474b1a
andrew-braun <andrew@andrewbraun.dev>
2023-07-28 15:01:05 +0400

    seo: Move google analytics script into _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Script from "next/script"
 import { Ubuntu } from "next/font/google"
 
 import Head from "next/head"
@@ -23,6 +24,15 @@ export default function MyApp({ Component, pageProps }) {
 			<Head>
 				<Favicon />
 			</Head>
+			<Script
+				strategy="afterInteractive"
+				id="google-tag-manager"
+			>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-5VDTQ4QT');`}</Script>
+
 			<style jsx global>{`
 				html {
 					font-family: ${ubuntu.style.fontFamily};
--- pages/_document.js
+++ pages/_document.js
@@ -4,18 +4,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
 	return (
 		<Html>
-			<Head>
-				<Script
-					strategy="afterInteractive"
-					dangerouslySetInnerHtml={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5VDTQ4QT');`,
-					}}
-				></Script>
-			</Head>
+			<Head></Head>
 			<body>
 				<noscript
 					dangerouslySetInnerHTML={{
