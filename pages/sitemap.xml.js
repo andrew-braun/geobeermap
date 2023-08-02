f02f180eed044a0f4bd68fd92df19c8ffca183de
andrew-braun <andrew@andrewbraun.dev>
2023-08-02 11:44:33 +0400

    chore: Remove extraneous text from sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -18,7 +18,6 @@ function generateSiteMap({ venues }) {
 
 	return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the URLs we know already-->
      <url>
        <loc>${siteData.url}</loc>
      </url>
